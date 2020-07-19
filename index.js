a1e861def21fbf373ca1615a12db26d7a5cdf4f8
Richard Chapman Gomez <chapman.gomez.richard@gmail.com>
2020-07-19 17:00:17 -0400

    updated readme

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const salt = new codehat.Salt();
 const connection = 'mongodb://127.0.0.1:27017/?readPreference=primary&ssl=false';
 
 let newSalt = salt.generate();
-let phrase = 'the lion &%* king walking down the jungle';
+//let phrase = 'the lion &%* king walking down the jungle';
 
 
 (async () => {
-    let encryption =  await codehat.Crypto.asyncKeySequenceEncrypt(phrase, false, {method: 'database', connectionString: connection, database: 'randomp', collection: 'keys'});
-    let decryption = await codehat.Crypto.asyncSequenceDecrypt(encryption.keyRing, encryption.sequence, undefined, {method: 'database', connectionString: connection, database: 'randomp', collection: 'keys'});
-    console.log('one time value', encryption.onetimeValues);
-    console.log('valor al azar:', phrase);
+    let encryption =  await codehat.Crypto.asyncKeySequenceEncrypt(newSalt, false, {method: 'database', connectionString: connection});
+    let decryption = await codehat.Crypto.asyncSequenceDecrypt(encryption.keyRing, encryption.sequence, undefined, {method: 'database', connectionString: connection});
+    console.log('valor al azar:', newSalt);
     console.log('encrypcion', encryption.keyRing);
     console.log('sequence', encryption.sequence);
     //console.log('', encryption.)
