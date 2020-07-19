d6284fa43c730126c3c682efc0dcbe4ddeb37d24
Richard Chapman Gomez <chapman.gomez.richard@gmail.com>
2020-07-19 17:09:38 -0400

    fixed typos

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ let newSalt = salt.generate();
 //let phrase = 'the lion &%* king walking down the jungle';
 
 
-(async () => {
-    let encryption =  await codehat.Crypto.asyncKeySequenceEncrypt(newSalt, false, {method: 'database', connectionString: connection});
-    let decryption = await codehat.Crypto.asyncSequenceDecrypt(encryption.keyRing, encryption.sequence, undefined, {method: 'database', connectionString: connection});
+
+    let encryption =  codehat.Crypto.keySequenceEncrypt(newSalt, false);
+    let decryption =  codehat.Crypto.keySequenceDecrypt(encryption.keyRing, encryption.sequence);
     console.log('valor al azar:', newSalt);
     console.log('encrypcion', encryption.keyRing);
     console.log('sequence', encryption.sequence);
     //console.log('', encryption.)
     console.log(decryption);
-})();
--- src/core/encryptions/cryptor.ts
+++ src/core/encryptions/cryptor.ts
@@ -15,8 +15,6 @@ export class Crypto {
             new KeyAndSequence();
         if (storageMethod.method === 'database') {
             const result = await this.keySequence.databaseProtocol();
-            console.log('keyRing', this.keySequence.equationValues);
-            console.log('database protocol', result);
             if(result) {
                 return this.keySequence.encrypt(keyRing);
             }
@@ -34,7 +32,7 @@ export class Crypto {
             this.keySequence.equationValues;
         return this.keySequence.decrypt(keyRing, sequence, onetimepadValues);
     }
-    static async asyncSequenceDecrypt(keyRing: any[], sequence: string[], onetimepadValues?: number[], storageMethod?: StorageMethod): Promise<string> {
+    static async asyncKeySequenceDecrypt(keyRing: any[], sequence: string[], onetimepadValues?: number[], storageMethod?: StorageMethod): Promise<string> {
         this.keySequence = onetimepadValues === undefined ? 
             new KeyAndSequence(false, storageMethod) :
             new KeyAndSequence();
@@ -43,7 +41,6 @@ export class Crypto {
             this.keySequence.equationValues;
         if(storageMethod.method === 'database') {
             const result = await this.keySequence.databaseProtocol();
-            console.log('decrypt equation values', this.keySequence.equationValues);
             if(result) {
                 return this.keySequence.decrypt(keyRing, sequence);
             }
